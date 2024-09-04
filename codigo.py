ca7adc182aa9cd11b769f99c4956937e6a6ca2fc
DevGBS01 <s.bispo@aluno.ifsp.edu.br>
2024-09-04 10:08:31 -0300

    clonenogit

--- codigo.py
+++ codigo.py
@@ -4,3 +4,11 @@ while(c<100000):
     n = n**2
     print(n)
     c = c+0.1
+    break
+
+#média de um aluno
+
+n1 = float(input("primeira nota do aluno:"))
+n2 = float(input('segunda nota do aluno:'))
+m  =  (n1+n2)/2
+print(f'a média do aluno é {m}')
